fe42ae20f9fcaff326df2d3af11b12339b20296f
user <user@DESKTOP-7EQF3M8>
2021-03-16 12:47:35 +0900

    03_16_A01_quiz

---
+++ First_03_16/src/quiz/A01_PrintQuiz.java
@@ -0,0 +1,28 @@
+package quiz;
+
+public class A01_PrintQuiz {
+
+	public static void main(String[] args) {
+		
+		//quiz_01 
+		System.out.println("===================");
+		System.out.println("       /)/)");
+		System.out.println("      (  ..)");
+		System.out.println("      (   >");
+		System.out.println(" Have a Good Time.");
+		System.out.println("===================\n");
+		
+		
+		//quiz_02
+		System.out.println("\t\t####회비 정보####");
+		System.out.println("==============================================");
+		System.out.println("이름\t\t나이\t전화번호\t\t회비");
+		System.out.println("==============================================");
+		System.out.println("홍길동\t\t15\t010-123-1234\t￦20,000");
+		System.out.println("임꺽정\t\t20\t010-123-1234\t￦30,000");
+		System.out.println("김말이\t\t15\t010-123-1234\t￦50,000");
+		System.out.println("----------------------------------------------");
+		System.out.println("총합계\t\t\t\t\t￦100,000");
+		System.out.println("==============================================");
+	}
+}
